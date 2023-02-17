c94f359672f7ffcf362cddcf501ca71f0530cc50
Sui <59013187+sui34@users.noreply.github.com>
2023-02-17 14:45:49 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //when you want to generate "random" number
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number inbetween 0 and 100 : ");
+
+                string s = Console.ReadLine();
+                //when you want to convert "s" to "i"
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too High!! Guess Lower ....");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too Low!! Guess Higher ....");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("Brilliant!! Congratulation!!");
+                    win = true;
+                }
+                Console.WriteLine();
+            } while (win == false);
+
+            Console.WriteLine("You have done well!!!");
+            Console.Write("Press any key to finish...");
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
